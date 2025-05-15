f7f2d26e5039ef9c42b447f8d6a9d02bd7a75410
Yura1546 <alanwalkes41@gmail.com>
2025-05-15 19:12:08 +0300

    Delete pythonProject1 directory

--- pythonProject1/shop/store/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import Clothing
-
-class ClothingFilter(django_filters.FilterSet):
-    class Meta:
-        model = Clothing
-        fields = {
-            'category': ['exact'],  # фільтрація за категорією
-        }
